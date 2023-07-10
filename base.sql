834dd7e70ffe6b930b1fe3a9ff3639b2058c957f
JohanKael <118510163+JohanKael@users.noreply.github.com>
2023-07-10 14:40:03 +0300

    Final base

--- base.sql
+++ base.sql
@@ -3,13 +3,12 @@ CREATE DATABASE fit;
 use fit;
 
 CREATE TABLE User (
-   id INT NOT NULL AUTO_INCREMENT,
+   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
    userName VARCHAR(50) NOT NULL,
    bday DATE,
    userEmail VARCHAR(255) NOT NULL,
    userPassword VARCHAR(255) NOT NULL
 );
-ALTER TABLE User ADD PRIMARY KEY(id);
 
 CREATE TABLE UserProfil (
    idUser INT,
@@ -20,28 +19,25 @@ CREATE TABLE UserProfil (
 ALTER TABLE UserProfil ADD FOREIGN KEY(idUser) references User(id);
 
 CREATE TABLE Objectif(
-    idObjectif INT NOT NULL AUTO_INCREMENT,
+    idObjectif INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     descriObjectif VARCHAR(40)
 );
-ALTER TABLE Objectif ADD PRIMARY KEY(idObjectif);
 
 CREATE TABLE Plat(
-    idPlat INT NOT NULL AUTO_INCREMENT, 
+    idPlat INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     nomPlat VARCHAR(30) NOT NULL,
     photoPlat VARCHAR(40),
     apportCalorique FLOAT NOT NULL,
     prixPlat FLOAT,
     idObjectif INT
 );
-ALTER TABLE Plat ADD PRIMARY KEY(idPlat);
 ALTER TABLE Plat ADD FOREIGN KEY(idObjectif) REFERENCES Objectif(idObjectif);
 
 CREATE TABLE Regime(
-    idRegime INT NOT NULL AUTO_INCREMENT,
+    idRegime INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     idObjectif INT NOT NULL,
     Duree INT
 );
-ALTER TABLE Regime ADD PRIMARY KEY(idRegime);
 ALTER TABLE Regime ADD FOREIGN KEY(idObjectif) REFERENCES Objectif(idObjectif);
 
 CREATE TABLE RegimePlat(
@@ -53,24 +49,23 @@ ALTER TABLE RegimePlat ADD FOREIGN KEY(idRegime) REFERENCES Regime(idRegime);
 ALTER TABLE RegimePlat ADD FOREIGN KEY(idPlat) REFERENCES Plat(idPlat);
 
 CREATE TABLE Activite(
-    idActivite INT NOT NULL AUTO_INCREMENT, 
+    idActivite INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     nomActivite VARCHAR(30) NOT NULL,
     descriActivite VARCHAR(40) NOT NULL, 
     idObjectif INT
 );
-ALTER TABLE Activite ADD PRIMARY KEY(idActivite);
 ALTER TABLE Activite ADD FOREIGN KEY(idObjectif) REFERENCES Objectif(idObjectif);
 
 CREATE TABLE UserWallet(
+    idWallet INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     idUser INT,
     montant FLOAT
 );
 ALTER TABLE UserWallet ADD FOREIGN KEY(idUser) REFERENCES User(id);
 
 CREATE TABLE MoneyCode(
-    idCode INT NOT NULL AUTO_INCREMENT,
+    idCode INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     descriCode VARCHAR(20),
     montantCode FLOAT,
     checkCode INT
 );
-ALTER TABLE MoneyCode ADD PRIMARY KEY(idCode);
