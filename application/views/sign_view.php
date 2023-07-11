243b0f56fec2e72106799381eb02bae3ec543ff5
JohanKael <118510163+JohanKael@users.noreply.github.com>
2023-07-11 10:12:38 +0300

    some adjustement on the signin page

--- application/views/sign_view.php
+++ application/views/sign_view.php
@@ -18,8 +18,8 @@
                 <input type="date" name="bday" id="input"><br>
                 <input type="number" name="userHeight" id="input" placeholder=" Please enter your height"><br>
                 <input type="number" name="userWeight" id="input" placeholder=" Please enter your weight"><br>
-                <input type="number" name="email" id="input" placeholder=" Please enter your E-mail"><br>
-                <input type="number" name="password" id="input" placeholder=" Please enter your password"><br>
+                <input type="text" name="email" id="input" placeholder=" Please enter your E-mail"><br>
+                <input type="password" name="password" id="input" placeholder=" Please enter your password"><br>
                 <p><input type="radio" name="userGender" value="homme">Homme</p>
                 <p><input type="radio" name="userGender" value="femme">Femme</p>
                 <input type="submit" value="Sign in" class="sign">
